ed6a70902174a1f782d2ea54340b5a77a4bc8ace
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-22 19:20:46 +0900

    add GetUserName

--- sources/comment.rb
+++ sources/comment.rb
@@ -1,14 +1,15 @@
 class Comment
   def initialize(comment_node)
     @comment_node = comment_node
-    @user_name = nil
+    @user_name = GetUserName()
     @favorite = nil
     @text = nil
     @color = nil
   end
 
   def GetUserName()
-
+    path = "div.status > div.status_right > a.status_name"
+    @comment_node.at(path).attribute("href").inner_text.split("/").last
   end
   #attr_reader :comment, :user_name, :favorite, :text, :color
 end
