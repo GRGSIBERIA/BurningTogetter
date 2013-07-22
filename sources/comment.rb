9dd62bdefdf74696f912b8eb055c30820cf42832
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-22 18:23:27 +0900

    add parameters

--- sources/comment.rb
+++ sources/comment.rb
@@ -1,3 +1,10 @@
 class Comment
+  def initialize()
+    @comment = nil
+    @user_name = nil
+    @favorite = nil
+    @text = nil
+    @color = nil
+  end
   attr_reader :comment, :user_name, :favorite, :text, :color
 end
