479b8621e51a8a4d843da96b1db51e2019d63edc
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-07-22 21:00:01 +0900

    add initialize

--- initialize.rb
+++ initialize.rb
@@ -1,3 +1,38 @@
 require 'sequel'
 
 db = Sequel.connect('sqlite://database.db')
+
+if db.table_exists?(:articles) then
+  db.drop_table(:articles)
+end
+
+db.create_table :articles do
+  primary_key :article_id
+  foreign_key :master_user_id, :users, :type=>Integer
+  Integer :view
+  Integer :favorite
+  index :article_id
+end
+
+if db.table_exists?(:comments) then
+  db.drop_table(:comments)
+end
+
+db.create_table :comments do
+  prymary_key :comment_id
+  foreign_key :article_id, :articles, :type=>Integer
+  foreign_key :user_id, :users, :type=>Integer
+  String :text
+  Integer :favorite
+  index :comment_id
+end
+
+if db.table_exists?(:comments) then
+  db.drop_table(:users)
+end
+
+db.create_table :users do
+  prymary_key :user_id
+  String :user_name
+  index :user_id
+end
